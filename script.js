461bac6fe35f183897f728dfad2a2d5d7bce0f39
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-26 10:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+var form = document.getElementById('form1');
 
+  // Getting the values of the first and last name input fields
+  var firstName = form.elements['fname'].value;
+  var lastName = form.elements['lname'].value;
+
+  // Combining the first and last name
+  var fullName = firstName + ' ' + lastName;
+
+  // Displaying the full name using an alert
+  alert(fullName);
 }
